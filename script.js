a59afa70d8ba6fe926b4e996bd4def5cb711b3b4
dubakularamya <150138715+dubakularamya@users.noreply.github.com>
2025-05-13 12:42:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+ let str = "aabbcdd";
+ let res = null;
+for (let char of str) {
+    if (
+        str.indexOf(char)
+        ===
+        str.lastIndexOf(char)
+    ) {
+        res = char;
+        break;
+    }
+}
+console.log("Input: " + str);
+console.log("Non-repeating character: " + res);
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
